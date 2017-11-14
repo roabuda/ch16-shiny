f737d240e02797fc3639e01ee704c219bd35ce97
Royce Abuda <roabuda@uw.edu>
2017-11-14 13:57:04 -0800

    complete exercise.R

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,9 +1,10 @@
 # Exercise 1: Loading functions
 
 # Set your directory
-
+setwd("~/info201/ch16-shiny/exercise-1")
 
 # Source your BuildScatter.r script, exposing your BuildScatter function
-
+source("./scripts/BuildScatter.R")
 
 # Use your BuildScatter function to draw a well labeled ggplot scatterplot of the iris data
+Scatter(iris, "iris", "blue", Sepal.Length, Sepal.Width, "Length", "Width")
