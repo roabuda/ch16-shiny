36da498f39e0affbdce5ec1c8876421f18fdce8f
tuckemeg000 <tuckemeg000@gmail.com>
2017-11-14 13:51:09 -0800

    Complete scatter plot function

--- exercise-1/scripts/BuildScatter.R
+++ exercise-1/scripts/BuildScatter.R
@@ -8,3 +8,9 @@
 #   - Title of the plot (set a default of "Title")
 #   - Label for the x axis (set a default of "X Title")
 #   - Label for the y axis (set a default of "Y Title")
+
+Scatter <- function(data, title, color, x, y, xlabel, ylabel) {
+  attach(data)
+  my.plot <- plot(x, y, main = title, xlab = xlabel, ylab = ylabel)
+  return(my.plot)
+}
